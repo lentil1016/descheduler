2f21327b0e4af0c3d9e75e1b6f74abfc3710d221
Sunyk <lentil1016@gmail.com>
2019-04-04 14:13:37 +0800

    fix incluster error

--- pkg/descheduler/client.go
+++ pkg/descheduler/client.go
@@ -2,6 +2,7 @@ package descheduler
 
 import (
 	"fmt"
+	"os"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -11,15 +12,15 @@ import (
 func CreateClient(kubeConfigFile string) (kubernetes.Interface, error) {
 	var cfg *rest.Config
 	var err error
-	if kubeConfigFile != "" {
-		cfg, err = clientcmd.BuildConfigFromFlags("", kubeConfigFile)
-		if err != nil {
-			return nil, fmt.Errorf("Failed to build config from kubeconfig file: %v ", err)
-		}
-	} else {
+	if _, err = os.Stat(kubeConfigFile); os.IsNotExist(err) {
 		cfg, err = rest.InClusterConfig()
 		if err != nil {
 			return nil, fmt.Errorf("Failed to build config from incluster config: %v", err)
+		}
+	} else {
+		cfg, err = clientcmd.BuildConfigFromFlags("", kubeConfigFile)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to build config from kubeconfig file: %v ", err)
 		}
 	}
 	return kubernetes.NewForConfig(cfg)
